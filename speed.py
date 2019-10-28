62cc4acba1ffb2e76c7c3657a354e2d25c2a63f0
sandheepp <sandheep.p01@gmail.com>
2019-10-28 22:26:04 +0530

    speed file added

--- speed.py
+++ speed.py
@@ -1,10 +1,7 @@
 from mtcnn.mtcnn import MTCNN
 import cv2
-import freenect
 import time
-import time
-
-
+# import freenect
 
 detector = MTCNN()
 image = cv2.cvtColor(cv2.imread("ivan.jpg"), cv2.COLOR_BGR2RGB)
@@ -19,28 +16,24 @@ cv2.rectangle(image,
               2)
 
 # For multiple faces
-# cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(0)
 
-previous = 0
+previous =0
 
 while True: 
     #Capture frame-by-frame
-    # __, frame = cap.read()
-    start = time.time()
+    __, frame = cap.read()
     
-
-    # (depth, _) = freenect.sync_get_depth()
-    (frame, _) = freenect.sync_get_video()
-
+    start = time.time()
     #Use MTCNN to detect faces
     result = detector.detect_faces(frame)
-    print(result)
+    # print(result)
     if result != []:
         for person in result:
             bounding_box = person['box']
             keypoints = person['keypoints']
-    
-            cv2.rectangle(frame,
+
+            frame = cv2.rectangle(frame,
                           (bounding_box[0], bounding_box[1]),
                           (bounding_box[0]+bounding_box[2], bounding_box[1] + bounding_box[3]),
                           (0,155,255),
@@ -51,13 +44,14 @@ while True:
             cv2.circle(frame,(keypoints['nose']), 2, (0,155,255), 2)
             cv2.circle(frame,(keypoints['mouth_left']), 2, (0,155,255), 2)
             cv2.circle(frame,(keypoints['mouth_right']), 2, (0,155,255), 2)
-    print(bounding_box[0] - previous) 
+    end = time.time()
+    time_taken = end- start
+    print("Speed (relative):",(bounding_box[0] - previous)/time_taken)
     previous = bounding_box[0];
-    print(previous)
+    # print(previous)
     #display resulting frame
     cv2.imshow('frame',frame)
-    print(time.time()-start)
     if cv2.waitKey(1) &0xFF == ord('q'):
         break#When everything's done, release capture
-# cap.release()
+cap.release()
 cv2.destroyAllWindows()
